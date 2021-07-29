ddf14de5507e5f240c8c16d496e19670e3b35f3d
David Okeke <iamdavonokeke@gmail.com>
2021-07-29 01:11:59 +0100

    first markup completed

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+
+var mobilemenulinks = document.querySelector('.mobilemenulinks');
+var button = document.querySelector('.mobileicon');
+
+button.addEventListener('click', function(){
+    mobilemenulinks.classList.toggle('open');
+})
